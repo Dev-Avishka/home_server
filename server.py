ca1c62641f33b856683423b92d1d453fa85d5156
Dev_Avishka <dabdissanayake@gmail.com>
2025-10-24 13:17:40 +0530

    lol

--- server.py
+++ server.py
@@ -9,16 +9,9 @@ def home():
     console.log("[bold green]ISIS : Service accessed[/bold green]")
     return "Hello, World!"
 
-@app.route('/calculate?eq=<equation>')
-def calculate(equation):
-    try:
-        result = eval(equation)
-        console.log(f"[bold blue]ISIS : Calculation performed: {equation} = {result}[/bold blue]")
-        return str(result)  
-    except Exception as e:
-        console.log(f"[bold red]ISIS Error in calculation: {e}[/bold red]")
-        return "Error in calculation", 400
-
+@app.route('/sendimage')
+def serve_image():
+    return flask.send_file('images/image.jpg', mimetype='image/jpeg')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
