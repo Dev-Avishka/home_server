5c5b1a86a330038a433726107f40ec9e88eb846d
Dev_Avishka <dabdissanayake@gmail.com>
2025-10-24 09:29:51 +0530

    fixed run

--- server.py
+++ server.py
@@ -7,3 +7,6 @@ console = Console()
 def home():
     console.log("ISIS : Service accessed")
     return "Hello, World!"
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
+    
